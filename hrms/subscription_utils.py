af4744f6ac287097e087093d1917141034af0c0c
Dmitriy <88440058+am-i-dead@users.noreply.github.com>
2025-03-28 15:35:49 +0500

    Update subscription_utils.py

--- hrms/subscription_utils.py
+++ hrms/subscription_utils.py
@@ -75,7 +75,7 @@ def update_erpnext_access(user_input: dict | None):
 	enables erpnext workspaces and roles if user has subscribed to both hrms and erpnext
 	disables erpnext workspaces and roles if user has subscribed to hrms but not erpnext
 	"""
-	if not frappe.utils.get_url().endswith(".life.aero"):
+	if not frappe.utils.get_url().endswith(".onboardsolutions.kz"):
 		return
 
 	update_erpnext_workspaces(True)
